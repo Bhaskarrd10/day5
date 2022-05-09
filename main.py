b388179cfff9eac5a71cca439ffc0c84c73a7d96
bhaskar reddy <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-09 17:37:20 +0000

    range

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ total = 0
 for number in range(1, 101):
   total += number
 print(total)
+
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 ==0:
+    print("FizzBuzz")
+  elif number  % 5 == 0:
+    print("Buzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  else:
+    print(number)
+
+
+
+    
