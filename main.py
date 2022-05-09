3c878ac7c90aeb805b42220fa3e31ce4ce1d0ccf
bhaskar reddy <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-09 17:20:15 +0000

    range function

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ fruits = ["apple", "orange", "mango"]
 for fruit in fruits:
   print(fruit)
 print(fruits)
+
+
+for number in range(1, 10):
+  print(number)
+
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
